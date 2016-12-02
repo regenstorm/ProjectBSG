bd82f24e00e03fe59754f87f92fd070380753c58
Trung Ngo <ndtrung4419@gmail.com>
2016-12-02 14:10:57 +0200

    Implement unit selection with pointer click

--- Unity/Assets/Scripts/Map.cs
+++ Unity/Assets/Scripts/Map.cs
@@ -5,11 +5,14 @@ public class Map : MonoBehaviour {
 	public int ScalingFactor = 2;
 	public int Width;
 	public int Height;
+	private GameObject unitsContainer;
+	private Unit selectedUnit;
 
 	// Use this for initialization
 	void Start () {
 		this.Width = ScalingFactor * 4;
 		this.Height = ScalingFactor * 3;
+		unitsContainer = transform.Find ("Units").gameObject;
 	}
 	
 	// Update is called once per frame
@@ -20,5 +23,24 @@ public class Map : MonoBehaviour {
 	public bool IsLegalPosition(Vector3 pos) {
 		return pos.x >= 0 && pos.x < Width 
 			&& pos.y <= 0 && pos.y > -Height;
+	}
+
+	public Unit UnitAtPosition(Vector3 pos) {
+		foreach (Transform child in unitsContainer.transform) {
+			if (child.localPosition == pos) {
+				return child.gameObject.GetComponent<Unit>();
+			}
+		}
+
+		return null;
+	}
+
+	public void SelectUnit(Unit unit) {
+		this.selectedUnit = unit;
+		unit.OnSelected ();
+	}
+
+	public void DeselectCurrentUnit() {
+		selectedUnit.OnDeselected ();
 	}
 }
--- Unity/Assets/Scripts/Unit.cs
+++ Unity/Assets/Scripts/Unit.cs
@@ -2,14 +2,34 @@
 using System.Collections;
 
 public class Unit : MonoBehaviour {
+	Transform shipSprite;
+	Map map;
+	bool selected = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		shipSprite = transform.FindChild ("ship_sprite");
+		map = GameObject.Find ("BattleGround").GetComponent<Map> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetMouseButtonDown (0)) {
+			if (!selected) {
+				map.SelectUnit (this);
+			} else {
+				map.DeselectCurrentUnit ();
+			}
+		}
+	}
+
+	public void OnSelected() {
+		this.selected = true;
+		shipSprite.Translate (new Vector3(0, 0.1f));
+	}
+
+	public void OnDeselected() {
+		this.selected = false;
+		shipSprite.Translate (new Vector3(0, -0.1f));
 	}
 }
