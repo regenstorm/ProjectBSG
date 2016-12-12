f3709dd27a7dd8e723f50f3926b7df3ff9af2413
Artem Abramov <ar.abramov@gmail.com>
2016-12-12 14:32:51 +0200

    Implemented placeholders for the Main Menu, Mission Selection Screen and Battle Intro.

---
+++ Unity/Assets/Scripts/Credits.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour {
+
+	public Button backButton;
+
+	// Use this for initialization
+	void Start () {
+		backButton.onClick.AddListener (BackButtonPressed);
+	}
+
+	public void BackButtonPressed () {
+		SceneManager.LoadScene ("MainMenu");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public Button newGameButton;
+	public Button exitGameButton;
+	public Button loadGameButton; //LOL
+	public Button creditsButton;
+
+	void Start () {
+		newGameButton.onClick.AddListener (NewGame);
+		exitGameButton.onClick.AddListener (ExitGame);
+		creditsButton.onClick.AddListener (LoadCredits);
+	}
+
+	public void NewGame() {
+		SceneManager.LoadScene ("MissionSelection");
+	}
+
+	public void LoadGame() {
+		//ROFL. Would never be called. NEVER.
+	}
+
+	public void LoadCredits() {
+		
+	}
+
+	public void ExitGame() {
+		Debug.Log ("Exiting game...");
+		Application.Quit();
+	}
+		
+}
